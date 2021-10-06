16d296a7726a3310ccec7f70f294a9ea11bf7d05
SarahFuziki <92062227+SarahFuziki@users.noreply.github.com>
2021-10-06 19:28:45 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+     <meta charset="utf-8">
+   <title>Título da página</title>
+   <link rel = "stylesheet" href="estilo.css" type="text/css">
+  </head>
+  <body class="estilo-azul">
+    Olá mundo.
+  </body>
+</html>
